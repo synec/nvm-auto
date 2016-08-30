bd76f69d542c464289a55cffc357c156dc5aa286
Daniel Grabert <dg@indivirtuell.net>
2016-08-30 15:39:56 +0200

    Code simplified, output of nvm use reactivated to show activated version on change

--- nvm-auto.plugin.zsh
+++ nvm-auto.plugin.zsh
@@ -1,24 +1,14 @@
 # Switch node version using nvm if the need arises
 function nvm_auto_switch {
-  unset NVM_RC_VERSION
-  CUR_VERSION=$(node -v |cut -c 2-)
-  nvm_rc_version > /dev/null 2>&1
-
-  if [ -z "${NVM_RC_VERSION}" ]
+  if [ -f .nvmrc ]
   then
-      export NVM_RC_VERSION=$(nvm_version default | cut -c 2-)
-  fi
-
-  if [ "v${NVM_RC_VERSION}" != "v${CUR_VERSION}" ]
-  then
-    nvm use >/dev/null 2>&1 || nvm use default >/dev/null 2>&1
+    nvm use
   fi
 }
 
 function nvm_auto_version {
   echo v1.0.0
 }
-
 
 autoload -U add-zsh-hook
 add-zsh-hook chpwd nvm_auto_switch
